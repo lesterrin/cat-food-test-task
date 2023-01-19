c5c704c5bba969b5af412c44529b35820aa01401
lesterrin <voldemar1810@gmail.com>
2023-01-19 17:52:20 +1000

    Сверстана карточка товара. Реализуется логика изменения состояния карточки.

--- src/cards/card/card.js
+++ src/cards/card/card.js
@@ -1,25 +1,53 @@
 import s from './card.module.css';
 import catImg from '../../assets/images/Photo.png';
+import React, {useState} from "react";
 
-const Card = ({taste, portions, gift,kgs}) => {
+const Card = ({taste, portions, weight, mouses, addit, status}) => {
+
+    const [state,setState] = useState({status: 'default'});
+
+    const onSetState = () => {
+        setState({
+            ...state,
+            status: 'selected'
+        })
+    }
+
+    let mouseText;
+
+    if (mouses === 1) mouseText = 'мышь';
+    if (mouses === 2) mouseText = <><b>{mouses}</b> мыши</>;
+    if (mouses > 2) mouseText = <><b>{mouses}</b> мышей</>;
+
+    const style = s[state.status];
+
     return (
-        <>
-            <div className={s.card_outside}>
+        <div>
+            <div className={`${s.card_outside} ${style}`}>
                 <div className={s.card_inside}>
                     <div className={s.description}>
                         <div className={s.caption}>Сказочное заморское яство</div>
                         <div className={s.title}>Нямушка</div>
                         <div className={s.taste}>{taste}</div>
-                        <div className={s.annotation}><b>{portions}</b> порций <br/> {gift}</div>
-                        <div className={s.kgs}>{kgs}</div>
+                        <div className={s.annotation}>
+                            <b>{portions}</b> порций <br/>
+                            {mouseText} в подарок <br/>
+                            {addit}
+                        </div>
+                        <div className={`${s.weight_round} ${style}`}>
+                            <div className={s.weight_wrapper}>
+                                <div className={s.weight}>{weight}</div>
+                                <div className={s.kgs}>кг</div>
+                            </div>
+                        </div>
                     </div>
                     <img className={s.cat_img} src={catImg}/>
                 </div>
             </div>
             <div className={s.appeal}>
-                Чего сидишь? Порадуй котэ, купи.
+                Чего сидишь? Порадуй котэ, <span className={s.blue}> <span className={s.buy} onClick={onSetState}>купи</span>.</span>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/cards/cards.js
+++ src/cards/cards.js
@@ -1,13 +1,18 @@
 import s from './cards.module.css';
 import Card from './card';
+import React from "react";
 
 const Cards = () => {
-    const cardsData = [{taste: 'с фуаг-ра', portions: 10, gift: 'мышь в подарок', kgs: '0,5'}];
-    const cards = cardsData.map(e => <Card {...e}/>);
+    const cardsData = [
+        {id: 1, taste: 'с фуаг-ра', portions: 10, mouses: 1, weight: '0,5', status: 'default'},
+        {id: 2, taste: 'с рыбой', portions: 40, mouses: 2, weight: '2', status: 'disabled'},
+        {id: 3, taste: 'с курой', portions: 100, mouses: 5, weight: '5', addit: 'заказчик доволен', status: 'selected'}
+    ];
+
+    const cards = cardsData.map(e => <Card key={e.id} {...e}/>);
     return (
         <>
-            <div>cards</div>
-            {cards}
+            <div className={s.cards_wrapper}>{cards}</div>
         </>
     );
 }
