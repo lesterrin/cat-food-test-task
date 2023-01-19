fb1972f5a816bf61ad7c210117c5f7e5457f40e3
vs_rin <voldemar1810@gmail.com>
2023-01-19 20:10:54 +1000

    Реализовано изменение состояний.

--- src/cards/card/card.js
+++ src/cards/card/card.js
@@ -2,9 +2,9 @@ import s from './card.module.css';
 import catImg from '../../assets/images/Photo.png';
 import React, {useState} from "react";
 
-const Card = ({taste, portions, weight, mouses, addit, status}) => {
+const Card = ({taste, portions, weight, mouses, addit, status, selectedAppeal}) => {
 
-    const [state,setState] = useState({status: 'default'});
+    const [state,setState] = useState({status: status});
 
     const onSetState = () => {
         setState({
@@ -18,6 +18,12 @@ const Card = ({taste, portions, weight, mouses, addit, status}) => {
     if (mouses === 1) mouseText = 'мышь';
     if (mouses === 2) mouseText = <><b>{mouses}</b> мыши</>;
     if (mouses > 2) mouseText = <><b>{mouses}</b> мышей</>;
+
+    let appeal;
+
+    if (state.status === 'default')  appeal = <>Чего сидишь? Порадуй котэ, <span className={s.blue}> <span className={s.buy} onClick={onSetState}>купи</span>.</span></>;
+    if (state.status === 'selected')  appeal = selectedAppeal;
+    if (state.status === 'disabled')  appeal = <div className={s.gold}>Печалька, с {taste} закончился</div>;
 
     const style = s[state.status];
 
@@ -34,18 +40,18 @@ const Card = ({taste, portions, weight, mouses, addit, status}) => {
                             {mouseText} в подарок <br/>
                             {addit}
                         </div>
-                        <div className={`${s.weight_round} ${style}`}>
-                            <div className={s.weight_wrapper}>
-                                <div className={s.weight}>{weight}</div>
-                                <div className={s.kgs}>кг</div>
-                            </div>
+                    </div>
+                    <div className={`${s.weight_round} ${style}`}>
+                        <div className={s.weight_wrapper}>
+                            <div className={s.weight}>{weight}</div>
+                            <div className={s.kgs}>кг</div>
                         </div>
                     </div>
                     <img className={s.cat_img} src={catImg}/>
                 </div>
             </div>
             <div className={s.appeal}>
-                Чего сидишь? Порадуй котэ, <span className={s.blue}> <span className={s.buy} onClick={onSetState}>купи</span>.</span>
+                {appeal}
             </div>
         </div>
     )
--- src/cards/cards.js
+++ src/cards/cards.js
@@ -4,9 +4,34 @@ import React from "react";
 
 const Cards = () => {
     const cardsData = [
-        {id: 1, taste: 'с фуаг-ра', portions: 10, mouses: 1, weight: '0,5', status: 'default'},
-        {id: 2, taste: 'с рыбой', portions: 40, mouses: 2, weight: '2', status: 'disabled'},
-        {id: 3, taste: 'с курой', portions: 100, mouses: 5, weight: '5', addit: 'заказчик доволен', status: 'selected'}
+        {
+            id: 1,
+            taste: 'с фуаг-ра',
+            portions: 10,
+            mouses: 1,
+            weight: '0,5',
+            status: 'default',
+            selectedAppeal: 'Печень утки разварная с артишоками'
+        },
+        {
+            id: 2,
+            taste: 'с рыбой',
+            portions: 40,
+            mouses: 2,
+            weight: '2',
+            status: 'default',
+            selectedAppeal: 'Головы щучьи с чесноком да свежайшая сёмгушка.'
+        },
+        {
+            id: 3,
+            taste: 'с курой',
+            portions: 100,
+            mouses: 5,
+            weight: '5',
+            status: 'disabled',
+            addit: 'заказчик доволен',
+            selectedAppeal: 'Филе из цыплят с трюфелями в бульоне.'
+        }
     ];
 
     const cards = cardsData.map(e => <Card key={e.id} {...e}/>);
